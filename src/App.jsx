70f78da0f33a11ecb8f1fccc17cc89ef29e3a969
Matt Pahuta <mhpahuta@gmail.com>
2025-08-12 16:38:13 -0700

    Add base component and data structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+
+import data from "./data/data.json";
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-4xl font-bold underline">Product List with Cart</h1>
-    </>
+    <div className="container max-w-7xl p-6">
+      <h1 className="text-4xl font-bold text-stone-900">Desserts</h1>
+    </div>
   )
 }
 
